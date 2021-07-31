18e21ca6c9b14fc23f1ee3c911b935e0b4430872
Loy van Beek <loy.vanbeek@gmail.com>
2021-07-31 14:26:38 +0200

    WIP: set up a dummy graph but planning in it kinda works

--- scripts/tap.py
+++ scripts/tap.py
@@ -18,42 +18,79 @@ class TopologicalActionPlanner:
         self._srv_plan = rospy.Service('~get_plan', Plan, self._srv_plan_cb)
         self._srv_update_edge = rospy.Service('~update_edge', Plan, self._srv_update_edge_cb)
 
-        nodes = []  # Or get all from ED?
-        edge_dicts = rospy.get_param('edges', [])  # type: List[Mapping]
-        for edge_dict in edge_dicts:
-            edge = Edge()
-            genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
+        # nodes = []  # Or get all from ED?
+        # edge_dicts = rospy.get_param('edges', [])  # type: List[Mapping]
+        # for edge_dict in edge_dicts:
+        #     edge = Edge()
+        #     genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
+        #
+        #     self.G.add_edge((edge.u.entity, edge.u.area),
+        #                     (edge.v.entity, edge.v.area),
+        #                     weight=edge.cost,
+        #                     action=edge.action)
 
-            self.G.add_edge((edge.u.entity, edge.u.area),
-                            (edge.v.entity, edge.v.area),
-                            weight=edge.cost,
-                            action=edge.action)
+        self.__generate_dummy_graph()
 
     def _srv_plan_cb(self, req):
         # type: (PlanRequest) -> PlanResponse
-        # path = nx.shortest_path(self.G,
-        #                         (req.origin.entity, req.origin.area),
-        #                         (req.destination.entity, req.destination.area))
 
-        a = Node(req.origin.entity, req.origin.area)
-        b = Node('inner_door', 'in_front_of')
-        c = Node('inner_door', 'in_back_of')
-        d = Node(req.destination.entity, req.destination.area)
-        edge_ab = Edge(u=a, v=b, cost=1, action_type=Edge.ACTION_DRIVE)
-        edge_bc = Edge(u=b, v=c, cost=10, action_type=Edge.ACTION_OPEN_DOOR)
-        edge_cd = Edge(u=c, v=d, cost=1, action_type=Edge.ACTION_DRIVE)
+        try:
+            path = nx.shortest_path(self.G,
+                                    (req.origin.entity, req.origin.area),
+                                    (req.destination.entity, req.destination.area))
 
-        return PlanResponse(success=True,
-                            edges=[edge_ab, edge_bc, edge_cd])
+            edges = []
+            for u, v in zip(path, path[1:]):
+                edges += [Edge(u=Node(*u),
+                               v=Node(*v),
+                               cost=self.G[u][v]['weight'],
+                               action_type=self.G[u][v]['action'])]
+            # TODO: for the drive edges, query the maybe now updated cost of driving that with current knowledge
+
+            return PlanResponse(success=True, edges=edges)
+        except nx.NetworkXNoPath as no_path_found_ex:
+            rospy.logerr(no_path_found_ex)
+            return PlanResponse(success=False)
+        except nx.NodeNotFound as node_not_found_ex:
+            rospy.logerr(node_not_found_ex)
+            return PlanResponse(success=False)
+
+    # TODO: rename u,v to src, dst
+    # error bool -> error code
+    # action types as strings
 
     def _srv_update_edge_cb(self):
         # type: (UpdateEdgeRequest) -> UpdateEdgeResponse
         pass
+
+    def __generate_dummy_graph(self, n_nodes=10, n_edges=20):
+        numbers = '0123456789'
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        import random
+        random.seed(123456789)
+        # Make a bunch of nodes with randomly picked names from numbers and letters
+        nodes = [(random.choice(numbers), random.choice(letters)) for _ in range(n_nodes)]
+
+        for _ in range(n_edges):
+            a, b = random.choice(nodes), random.choice(nodes)
+            if a == b:
+                continue
+            self.G.add_edge(a,
+                            b,
+                            weight=random.random(),
+                            # 60% chance of driving, 30% doors, 10% pushing:
+                            action=random.choice([Edge.ACTION_DRIVE]*6 +
+                                                 [Edge.ACTION_OPEN_DOOR]*3 +
+                                                 [Edge.ACTION_DRIVE]))
 
 
 if __name__ == "__main__":
     rospy.init_node("topological_action_planner")
 
     tap = TopologicalActionPlanner()
+
+    # import matplotlib.pyplot as plt
+    # nx.draw(tap.G, with_labels=True)
+    # plt.show()
 
     rospy.spin()
