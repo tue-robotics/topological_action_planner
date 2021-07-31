f46f61c7cfad1637e593d5eb568501943b8b5a47
Loy van Beek <loy.vanbeek@gmail.com>
2021-07-31 15:23:31 +0200

    Add example graph definition, which can be loaded with
    rosparam load tests/example_graph.yaml /topological_action_planner/edges
    and then queried with rosservice call /topological_action_planner/getPlan ...

--- topological_action_planner/scripts/tap.py
+++ topological_action_planner/scripts/tap.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python
+import os
+
+import yaml
 
 import rospy
 import genpy
@@ -19,17 +22,24 @@ class TopologicalActionPlanner:
         self._srv_update_edge = rospy.Service('~update_edge', Plan, self._srv_update_edge_cb)
 
         # nodes = []  # Or get all from ED?
-        # edge_dicts = rospy.get_param('edges', [])  # type: List[Mapping]
-        # for edge_dict in edge_dicts:
-        #     edge = Edge()
-        #     genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
-        #
-        #     self.G.add_edge((edge.origin.entity, edge.origin.area),
-        #                     (edge.destination.entity, edge.destination.area),
-        #                     weight=edge.cost,
-        #                     action=edge.action)
+        edge_dicts = rospy.get_param('~edges', [])  # type: List[Mapping]
+        for edge_dict in edge_dicts:
+            edge = Edge()
+            genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
 
-        self.__generate_dummy_graph()
+            self.G.add_edge((edge.origin.entity, edge.origin.area),
+                            (edge.destination.entity, edge.destination.area),
+                            weight=edge.cost,
+                            action_type=edge.action_type)
+
+        # self.__generate_dummy_graph()
+        # self.serialize_graph('/tmp/graph.yaml')
+
+        # import matplotlib.pyplot as plt
+        # nx.draw(self.G, with_labels=True)
+        # plt.ion()
+        # plt.show()
+        # plt.pause(0.001)
 
     def _srv_plan_cb(self, req):
         # type: (PlanRequest) -> PlanResponse
@@ -44,7 +54,7 @@ class TopologicalActionPlanner:
                 edges += [Edge(origin=Node(*u),
                                destination=Node(*v),
                                cost=self.G[u][v]['weight'],
-                               action_type=self.G[u][v]['action'])]
+                               action_type=self.G[u][v]['action_type'])]
             # TODO: for the drive edges, query the maybe now updated cost of driving that with current knowledge
 
             return PlanResponse(error_msg='', error_code=PlanResponse.SUCCESS, edges=edges)
@@ -58,6 +68,23 @@ class TopologicalActionPlanner:
     def _srv_update_edge_cb(self):
         # type: (UpdateEdgeRequest) -> UpdateEdgeResponse
         pass
+
+    def serialize_graph(self, filename):
+        file_dir = os.path.dirname(filename)
+        if not os.path.exists(file_dir):
+            os.mkdir(file_dir)
+
+        with open(filename, 'w') as storage:  # type: file
+            edges = []
+            for nx_edge in self.G.edges.items():
+                edge_dict = {'origin': {'entity': nx_edge[0][0][0],
+                                        'area': nx_edge[0][0][1]},
+                             'destination': {'entity': nx_edge[0][1][0],
+                                             'area': nx_edge[0][1][1]},
+                             'cost': nx_edge[1]['weight'],
+                             'action_type': nx_edge[1]['action_type']}
+                edges += [edge_dict]
+            yaml.dump(edges, storage)
 
     def __generate_dummy_graph(self, n_nodes=10, n_edges=20):
         numbers = '0123456789'
@@ -75,18 +102,14 @@ class TopologicalActionPlanner:
                             b,
                             weight=random.random(),
                             # 60% chance of driving, 30% doors, 10% pushing:
-                            action=random.choice([Edge.ACTION_DRIVE]*6 +
-                                                 [Edge.ACTION_OPEN_DOOR]*3 +
-                                                 [Edge.ACTION_DRIVE]))
+                            action_type=random.choice([Edge.ACTION_DRIVE]*6 +
+                                                      [Edge.ACTION_OPEN_DOOR]*3 +
+                                                      [Edge.ACTION_DRIVE]))
 
 
 if __name__ == "__main__":
     rospy.init_node("topological_action_planner")
 
     tap = TopologicalActionPlanner()
-
-    # import matplotlib.pyplot as plt
-    # nx.draw(tap.G, with_labels=True)
-    # plt.show()
 
     rospy.spin()
