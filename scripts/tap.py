abc1d51139314c6a269ecbda1a129fed5c9ac49a
Loy van Beek <loy.vanbeek@gmail.com>
2021-07-31 12:31:54 +0200

    Implement mocked get_plan service

--- scripts/tap.py
+++ scripts/tap.py
@@ -15,11 +15,11 @@ class TopologicalActionPlanner:
     def __init__(self):
         self.G = nx.Graph()
 
-        self._srv_plan = rospy.Service('plan', Plan, self._srv_plan_cb)
+        self._srv_plan = rospy.Service('get_plan', Plan, self._srv_plan_cb)
         self._srv_update_edge = rospy.Service('update_edge', Plan, self._srv_update_edge_cb)
 
         nodes = []  # Or get all from ED?
-        edge_dicts = rospy.get_param('edges')  # type: List[Mapping]
+        edge_dicts = rospy.get_param('edges', [])  # type: List[Mapping]
         for edge_dict in edge_dicts:
             edge = Edge()
             genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
@@ -31,12 +31,22 @@ class TopologicalActionPlanner:
 
     def _srv_plan_cb(self, req):
         # type: (PlanRequest) -> PlanResponse
-        path = nx.shortest_path(self.G,
-                                (req.origin.entity, req.origin.area),
-                                (req.destination.entity, req.destination.area))
-        path =
+        # path = nx.shortest_path(self.G,
+        #                         (req.origin.entity, req.origin.area),
+        #                         (req.destination.entity, req.destination.area))
 
-    def _srv_update_edge(self):
+        a = Node('table', 'in_front_of')
+        b = Node('inner_door', 'in_front_of')
+        c = Node('inner_door', 'in_back_of')
+        d = Node('kitchen', 'in_front_of')
+        edge_ab = Edge(u=a, v=b, cost=1, action_type=Edge.ACTION_DRIVE)
+        edge_bc = Edge(u=b, v=c, cost=10, action_type=Edge.ACTION_OPEN_DOOR)
+        edge_cd = Edge(u=c, v=d, cost=1, action_type=Edge.ACTION_DRIVE)
+
+        return PlanResponse(success=True,
+                            edges=[edge_ab, edge_bc, edge_cd])
+
+    def _srv_update_edge_cb(self):
         # type: (UpdateEdgeRequest) -> UpdateEdgeResponse
         pass
 
