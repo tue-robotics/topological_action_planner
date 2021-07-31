e4dbff8d401d35e1772d85b0f9e5466f597d2ab7
Loy van Beek <loy.vanbeek@gmail.com>
2021-07-31 16:08:08 +0200

    Move some serialisation and deserialisation to a separate file

--- topological_action_planner/scripts/tap.py
+++ topological_action_planner/scripts/tap.py
@@ -13,36 +13,27 @@ from topological_action_planner_msgs.msg import Edge, Node
 from topological_action_planner_msgs.srv import Plan, PlanRequest, PlanResponse
 from topological_action_planner_msgs.srv import UpdateEdge, UpdateEdgeRequest, UpdateEdgeResponse
 
+from topological_action_planner.serialisation import from_dicts
+
 
 class TopologicalActionPlanner:
     def __init__(self):
-        self.G = nx.Graph()
+        self.G = from_dicts(rospy.get_param('~edges'))
 
         self._srv_plan = rospy.Service('~get_plan', Plan, self._srv_plan_cb)
         self._srv_update_edge = rospy.Service('~update_edge', Plan, self._srv_update_edge_cb)
 
-        # nodes = []  # Or get all from ED?
-        edge_dicts = rospy.get_param('~edges', [])  # type: List[Mapping]
-        for edge_dict in edge_dicts:
-            edge = Edge()
-            genpy.message.fill_message_args(edge, [edge_dict])  # Convert structure in nested dict to ROS msg
-
-            self.G.add_edge((edge.origin.entity, edge.origin.area),
-                            (edge.destination.entity, edge.destination.area),
-                            weight=edge.cost,
-                            action_type=edge.action_type)
-
         # self.__generate_dummy_graph()
-        # self.serialize_graph('/tmp/graph.yaml')
+        # to_yaml(self.G, '/tmp/graph.yaml')
 
-        # import matplotlib.pyplot as plt
-        # nx.draw(self.G, with_labels=True)
-        # plt.ion()
-        # plt.show()
-        # plt.pause(0.001)
+        import matplotlib.pyplot as plt
+        nx.draw(self.G, with_labels=True)
+        plt.ion()
+        plt.show()
+        plt.pause(0.001)
 
     def _srv_plan_cb(self, req):
-        # type: (PlanRequest) -> PlanResponse
+        # type: (PlanRequest) -> PlanRespons
 
         try:
             path = nx.shortest_path(self.G,
@@ -68,23 +59,6 @@ class TopologicalActionPlanner:
     def _srv_update_edge_cb(self):
         # type: (UpdateEdgeRequest) -> UpdateEdgeResponse
         pass
-
-    def serialize_graph(self, filename):
-        file_dir = os.path.dirname(filename)
-        if not os.path.exists(file_dir):
-            os.mkdir(file_dir)
-
-        with open(filename, 'w') as storage:  # type: file
-            edges = []
-            for nx_edge in self.G.edges.items():
-                edge_dict = {'origin': {'entity': nx_edge[0][0][0],
-                                        'area': nx_edge[0][0][1]},
-                             'destination': {'entity': nx_edge[0][1][0],
-                                             'area': nx_edge[0][1][1]},
-                             'cost': nx_edge[1]['weight'],
-                             'action_type': nx_edge[1]['action_type']}
-                edges += [edge_dict]
-            yaml.dump(edges, storage)
 
     def __generate_dummy_graph(self, n_nodes=10, n_edges=20):
         numbers = '0123456789'
