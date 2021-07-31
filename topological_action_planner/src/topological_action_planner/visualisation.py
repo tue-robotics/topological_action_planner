d653c8476e638edffdaf4278dd05fbe1a66e8f0a
Lars Janssen <janslar@hotmail.com>
2021-07-31 16:44:22 +0200

    Dummy for Loy

--- topological_action_planner/src/topological_action_planner/visualisation.py
+++ topological_action_planner/src/topological_action_planner/visualisation.py
@@ -1,22 +1,9 @@
-# markerarray = MarkerArray()
-# for i in range(10):
-#     mark = Marker()
-#     mark.header.stamp = rospy.Time.now()
-#     mark.header.frame_id = "map"
-#     mark.type = Marker.CUBE
-#     mark.id = i
-#     mark.scale.x = 0.5
-#     mark.scale.y = 0.5
-#     mark.color.r = 1
-#     mark.color.g = 0
-#     mark.color.b = 0
-#     mark.color.a = 1
-#     mark.pose.position.x = i
-#     mark.pose.position.y = i
-#     markerarray.markers.append(mark)
-import rosparam
+from visualization_msgs.msg import MarkerArray
 
-rosparam.load_file()
 
-def create_tap_markerarray():
-    
+def create_tap_marker_array(graph):
+
+    # nodes = graph.nodes
+    # edges = graph.edges
+    markerarray = MarkerArray()
+    return markerarray
