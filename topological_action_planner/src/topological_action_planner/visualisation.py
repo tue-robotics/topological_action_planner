fa9c19744beb13212dfdf4538cfa37439fb718ef
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-08-09 00:03:57 +0200

    Small fixes in visualisation

--- topological_action_planner/src/topological_action_planner/visualisation.py
+++ topological_action_planner/src/topological_action_planner/visualisation.py
@@ -8,7 +8,10 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
     marker_array = MarkerArray()
     stamp = rospy.Time.now()
 
-    for i, (entity, area) in enumerate(graph.nodes.keys()):
+    for i, (entity_id, area) in enumerate(graph.nodes.keys()):
+        entity = wm.get_entity(entity_id)
+        if entity is None:
+            continue
         n_mark = Marker()
         n_mark.header.stamp = stamp
         n_mark.header.frame_id = frame
@@ -20,7 +23,7 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
         n_mark.scale.z = 0.1
         n_mark.color.a = 1
         n_mark.color.r = 1
-        n_mark.pose = wm.get_entity(entity).volumes[area].center_point
+        n_mark.pose = wm.get_entity(entity_id).volumes[area].center_point
         marker_array.markers.append(n_mark)
 
     for j, (origin, destination) in enumerate(graph.edges.keys()):
@@ -37,7 +40,7 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
         e_mark.color.a = 1
         e_mark.pose.orientation.w = 1  # To squelch annoying messages about uninitialized quats
 
-        vec1 = wm.get_entity(origin[0]).volumes[origin[1]].center_point,
+        vec1 = wm.get_entity(origin[0]).volumes[origin[1]].center_point
         vec2 = wm.get_entity(destination[0]).volumes[destination[1]].center_point
         e_mark.points = [Point(vec1.x, vec1.y, vec1.z),
                          Point(vec2.x, vec2.y, vec2.z)]
