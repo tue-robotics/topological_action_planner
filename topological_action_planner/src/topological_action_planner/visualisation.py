07cf39cbe5d4aa38a303d45c54d74325e54181a2
Loy van Beek <loy.vanbeek@gmail.com>
2021-09-21 20:15:45 +0200

    Vizualize edges in two directions

--- topological_action_planner/src/topological_action_planner/visualisation.py
+++ topological_action_planner/src/topological_action_planner/visualisation.py
@@ -1,3 +1,5 @@
+import copy
+
 import rospy
 from visualization_msgs.msg import MarkerArray, Marker
 from ed_py.world_model import WM
@@ -10,6 +12,14 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
     stamp = rospy.Time.now()
     rgb_list = {"DRIVE": [0, 0, 1], "OPEN_DOOR": [0, 1, 0], "PUSH_OBJECT": [1, 1, 0]}
 
+    def infinity():
+        x = 0
+        while True:
+            yield x
+            x += 1
+
+    inf = infinity()
+
     for i, (entity_id, area) in enumerate(graph.nodes.keys()):
         entity = wm.get_entity(entity_id)
         if entity is None:
@@ -19,21 +29,24 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
         n_mark.header.frame_id = frame
         n_mark.type = Marker.SPHERE
         n_mark.ns = "nodes"
-        n_mark.id = i
+        n_mark.id = next(inf)
         n_mark.scale.x = 0.2
         n_mark.scale.y = 0.2
         n_mark.scale.z = 0.2
         n_mark.color.a = 1
         n_mark.color.r = 1
-        # TODO: Copy logic to transform center_points to /map from the edges below.
+
+        vec = wm.tf_buffer.transform(
+            VectorStamped(
+                entity.volumes[area].center_point, frame_id=entity.uuid, stamp=rospy.Time(0)
+            ),
+            "map",
+        )
         n_mark.pose = Pose(
-            position=Vector3(
-                entity.volumes[area].center_point.x(),
-                entity.volumes[area].center_point.y(),
-                entity.volumes[area].center_point.y(),
+                position=Vector3(vec.vector.x(), vec.vector.y(), vec.vector.z()
             ),
             orientation=Quaternion(0, 0, 0, 1),
-        )  # TODO: convert to Pose
+        )
         marker_array.markers.append(n_mark)
 
     for j, (origin, destination) in enumerate(graph.edges.keys()):
@@ -42,13 +55,13 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
         e_mark.header.frame_id = frame
         e_mark.type = Marker.ARROW
         e_mark.ns = "edges"
-        e_mark.id = j + len(graph.nodes.keys())
+        e_mark.id = next(inf)
         edge_info = graph.edges[origin, destination]
         color_vec = rgb_list[edge_info["action_type"]]
         line_weight = max(min(0.01 * edge_info["weight"], 0.01), 0.1)
         e_mark.scale.x = line_weight
         e_mark.scale.y = line_weight * 2
-        e_mark.scale.z = line_weight
+        e_mark.scale.z = line_weight * 2
         e_mark.color.r = color_vec[0]
         e_mark.color.g = color_vec[1]
         e_mark.color.b = color_vec[2]
@@ -84,6 +97,11 @@ def create_tap_marker_array(graph, wm: WM, frame="map"):
             Point(vec2.vector.x(), vec2.vector.y(), vec2.vector.z()),
         ]
         marker_array.markers.append(e_mark)
+
+        reversed_arrow = copy.deepcopy(e_mark)
+        reversed_arrow.id = next(inf)
+        reversed_arrow.points = list(reversed(reversed_arrow.points))
+        marker_array.markers.append(reversed_arrow)
 
         rospy.loginfo("Add edge {} -> {}".format(origin, destination))
         rospy.loginfo("{} -> {}".format(origin_entity, destination_entity))
